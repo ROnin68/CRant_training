405ce2458216d54cb217827c141207d4ce358a0d
Oleg Rokach <Oleg.Rokach@gmail.com>
2017-10-31 11:33:08 +0200

    ClientNamesFilter has been added

---
+++ Task_ASP/AppFacade/Src/ClientNamesFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task_ASP.AppFacade
+{
+    public class ClientNamesFilter
+    {
+        public bool NameIsAllowed(string name)
+        {
+            Match match = Regex.Match(name, "[<>#]", RegexOptions.IgnoreCase);
+
+            return !match.Success;
+        }
+    }
+}
